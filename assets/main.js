9c9a819022bb29edfeb4789d40b094de248496e0
Ibrahim Joshua <IbrahimJ@C-GMEK012.collett.local>
2023-01-13 13:09:42 +0100

    'second-commit'

---
+++ assets/main.js
@@ -0,0 +1,1 @@
+console.warn("get ready to go people");
--- index.php
+++ index.php
@@ -4,19 +4,13 @@
 *
 *@package Aquila
  */
-
+get_header();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wordpress Theme</title>
-</head>
-<body>
-hello Wordpress
-    
-</body>
-</html>
+
+<div class="content">
+Content
+</div>
+
+
+<?php get_footer(); ?>
